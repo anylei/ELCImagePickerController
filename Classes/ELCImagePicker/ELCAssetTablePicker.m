d4c09112e054c33606e7d08eb9de846f64bc6df6
anylei <4nylei@gmail.com>
2013-08-12 18:45:23 +0800

    fix merge

--- Classes/ELCImagePicker/ELCAssetTablePicker.m
+++ Classes/ELCImagePicker/ELCAssetTablePicker.m
@@ -11,8 +11,6 @@
 #import "ELCAlbumPickerController.h"
 
 @interface ELCAssetTablePicker () {
-    UILabel *selectedLabel; /** Label to indicate how many images have been selected */
-    UILabel *instructionLabel; /** Label to indicate whether to change selection or press done */
     NSInteger selectedCount; /** Count of selected images */
 }
 
@@ -34,16 +32,8 @@
 
 - (void)viewDidLoad
 {
-    //Customize navigation controller
-    [self.navigationController customizeNavigationbarForLimn];
-
-    //Customize tableview
-    self.tableView.separatorStyle = UITableViewCellSeparatorStyleNone;
-    self.tableView.backgroundColor = [UIColor clearColor];
-
     [self.tableView setSeparatorStyle:UITableViewCellSeparatorStyleNone];
     [self.tableView setAllowsSelection:NO];
-    //headerView = [self createHeaderView];
 
     NSMutableArray *tempArray = [[NSMutableArray alloc] init];
     self.elcAssets = tempArray;
@@ -61,65 +51,6 @@
     }
 
 	[self performSelectorInBackground:@selector(preparePhotos) withObject:nil];
-}
-
-- (UITableView*)tableView
-{
-    return tableView;
-}
-
-- (void)setTableView:(UITableView *)newTableView
-{
-    if ( newTableView != tableView )
-    {
-        [tableView release];
-        tableView = [newTableView retain];
-    }
-}
-
-- (void)loadView {
-    [super loadView];
-    //save current tableview, then replace view with a regular uiview
-    self.tableView = (UITableView*)self.view;
-    UIView *replacementView = [[UIView alloc] initWithFrame:self.tableView.frame];
-    self.view = replacementView;
-    [replacementView release];
-    [self.view addSubview:self.tableView];
-
-    //code below adds some custom stuff above the table
-    UIView *customHeader = [self createHeaderView];
-    [self.view addSubview:customHeader];
-    [customHeader release];
-
-    //Set background
-    self.view.backgroundColor = [UIColor colorWithPatternImage:[UIImage imageNamed:@"background_pattern"]];
-
-    self.tableView.frame = CGRectMake(0, customHeader.frame.size.height, self.view.frame.size.width, self.view.frame.size.height - customHeader.frame.size.height);
-}
-
-- (UIView*)createHeaderView {
-    UIView *header = [[UIView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, 44)];
-    header.backgroundColor = [UIColor clearColor];
-
-    //Selection label
-    selectedLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 0, 320, 21)] autorelease];
-    selectedLabel.backgroundColor = [UIColor clearColor];
-    selectedLabel.textColor = [UIColor whiteColor];
-    selectedLabel.font = [UIFont fontWithName:@"Helvetica-Light" size:12.0];
-    selectedLabel.textAlignment = NSTextAlignmentCenter;
-    selectedLabel.text = [NSString stringWithFormat:NSLocalizedString(@"You have selected %d photos", @"Count of selected photos on image picker screen"),0];
-    [header addSubview:selectedLabel];
-
-    //Instruction label
-    instructionLabel = [[[UILabel alloc] initWithFrame:CGRectMake(0, 21, 320, 21)] autorelease];
-    instructionLabel.backgroundColor = [UIColor clearColor];
-    instructionLabel.textColor = [UIColor whiteColor];
-    instructionLabel.font = [UIFont fontWithName:@"Helvetica-Light" size:12.0];
-    instructionLabel.textAlignment = NSTextAlignmentCenter;
-    instructionLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Please select %d more photos", @"Select more photos label on image picker screen"),minimumSelection];
-    [header addSubview:instructionLabel];
-
-    return header;
 }
 
 - (void)viewWillAppear:(BOOL)animated
@@ -305,29 +236,12 @@
             }
         }
     }
-
-    selectedLabel.text = [NSString stringWithFormat:NSLocalizedString(@"You have selected %d photos", @"Count of selected photos on image picker screen"),selectedCount];
-
-    if ((minimumSelection > 0) && (selectedCount < minimumSelection)) {
-        instructionLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Please select %d more photos", @"Select more photos label on image picker screen"),minimumSelection-selectedCount];
-        self.navigationItem.rightBarButtonItem.enabled = NO;
-    }
-    else if ((maximumSelection > 0) && (selectedCount > maximumSelection)) {
-        instructionLabel.text = [NSString stringWithFormat:NSLocalizedString(@"Please deselect %d photos", @"Deselect photos label on image picker screen"),selectedCount-maximumSelection];
-        self.navigationItem.rightBarButtonItem.enabled = NO;
-    }
-    else {
-        instructionLabel.text = NSLocalizedString(@"You may press Done to complete", @"Reached desired amount of selected image on image picker screen");
-        self.navigationItem.rightBarButtonItem.enabled = YES;
-    }
-
 }
 
 // Image selection delegate
 
 - (NSNumber*)canSelectMore {
-    return [NSNumber numberWithBool:YES];
-    //return [NSNumber numberWithBool:((maximumSelection == 0) || (selectedCount < maximumSelection))];
+    return [NSNumber numberWithBool:((maximumSelection == 0) || (selectedCount < maximumSelection))];
 }
 
 @end
